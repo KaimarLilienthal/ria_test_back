074ecbd9f7e56adb66edafb1c3d9812590b2f58c
Kaimar <kaimarlilienthal@gmail.com>
2023-09-05 18:12:15 +0300

    Initial commit

---
+++ src/main/java/com/example/ria_test_back/RiaTestBackApplication.java
@@ -0,0 +1,13 @@
+package events.ee_ria_test_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RiaTestBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RiaTestBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ria_test_back/RiaTestBackApplicationTests.java
@@ -0,0 +1,13 @@
+package events.ee_ria_test_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RiaTestBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
